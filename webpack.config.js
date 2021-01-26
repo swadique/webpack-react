599da565108b62d6fd918951a9f12dc4bb9878ce
swadique <swadique@dexlock.com>
2021-01-26 15:00:14 +0530

    Saved

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "app", "index.js") },
+  output: { path: path.resolve(__dirname, "build") },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "app", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+
+    ]
+  },
+};
